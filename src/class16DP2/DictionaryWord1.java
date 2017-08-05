847a1c685fe030ee3a8ef3edb5a309017ca09f54
YifengGuo <gyf940517@gmail.com>
2017-08-05 16:08:36 -0400

    some correctness and comment

--- src/class16DP2/DictionaryWord1.java
+++ src/class16DP2/DictionaryWord1.java
@@ -20,19 +20,30 @@ import java.util.Set;
 	
 	Word: “robcatbob” return true since it can be composed by "rob", "cat", "bob"
  */
+
+/*
+ * need to remember when invoke String.substring(a,b) this 
+ * api would cause O(n) time so time complexity is O(n ^ 3)
+ * 
+ * and first limit a is included and 
+ * second limit b is excluded!!!
+ * 
+ * so we here use input.substring(0, i + 1) to represent [0,i]
+ */
 public class DictionaryWord1 {
 	public boolean canBreak(String input, String[] dict) {
 		Set<String> dictSet = toSet(dict);
-		boolean[] M = new boolean[input.length() + 1];
-		// base case
-		M[0] = false;
-		for (int i = 1; i < M.length; i++) {
-			if (dictSet.contains(input.substring(0, i))) {//make sure the whole sequence will be checked 
+		boolean[] M = new boolean[input.length()];
+
+		for (int i = 1; i < input.length(); i++) {
+			if (dictSet.contains(input.substring(0, i + 1))) {//make sure the whole sequence will be checked 
 				M[i] = true;
-				continue;//continue to next iteration
+				continue;//continue to next iteration of outer loop
 			}
-			for (int j = 1; j < i; j++) {//to check at current i, if there exists a j before i that guarantee substring(0,j) is a word and substring(j,i) is a word
-				if (M[j] && dictSet.contains(input.substring(j, i))) {
+			for (int j = 1; j < i; j++) { // to check at current i, if there exists a j before 
+									      // i that guarantee substring(0,j+1) 
+										  // is a word and substring(j+1,i+1) is a word
+				if (M[j] && dictSet.contains(input.substring(j + 1, i + 1))) {
 					M[i] = true; // if so, (0,i) can break to certain number of words
 					break;// already found certain j,terminal the inner for loop, move to next i
 				}
