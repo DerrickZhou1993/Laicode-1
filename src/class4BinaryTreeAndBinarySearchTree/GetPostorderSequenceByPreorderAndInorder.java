8976503ad782fecb9988a754d928515e5c21369a
YifengGuo <gyf940517@gmail.com>
2017-08-18 12:28:43 -0400

    Self Practice Binary Tree And Recursion

--- src/class4BinaryTreeAndBinarySearchTree/GetPostorderSequenceByPreorderAndInorder.java
+++ src/class4BinaryTreeAndBinarySearchTree/GetPostorderSequenceByPreorderAndInorder.java
@@ -1,5 +1,35 @@
 package class4BinaryTreeAndBinarySearchTree;
+/**
+ * 
+ * @author @Yifeng
+ * 
+ * Given Inorder and Preorder traversals of a binary tree, get the Postorder traversal without reconstructing a binary tree.
 
+	Assumptions:
+	
+	The given Inorder and Preorder traversals are guaranteed to be valid.
+	Examples:
+	
+	Input:
+	
+	Inorder traversal in[] = {4, 2, 5, 1, 3, 6}
+	Preorder traversal pre[] = {1, 2, 4, 5, 3, 6}
+	
+	Output:
+	Postorder traversal is {4, 5, 2, 6, 3, 1}
+ */
+
+/*
+   The idea is, root is always the first item in preorder traversal and it must be the last item in postorder traversal. 
+   We first recursively print left subtree, then recursively print right subtree. Finally, print root. 
+  
+   To find boundaries of left and right subtrees in pre[] and in[], we search root in in[], all elements before root in in[] are 
+   elements of left subtree and all elements after root are elements of right subtree. 
+   
+   In pre[], all elements after index of root in in[] are elements of right subtree. And elements before index 
+   (including the element at index and excluding the first element) are elements of left subtree.
+
+ */
 public class GetPostorderSequenceByPreorderAndInorder {
 	public int[] postOrder(int[] pre, int[] in) {
 		int[] result = new int[pre.length];
