870b76b8c8f9ec523664fe9427f796495f59deb0
YifengGuo <gyf940517@gmail.com>
2017-08-21 16:04:32 -0400

    Advanced Class 7 DP4

--- src/advanced_class7_DP4/LargestSetOfPointsWithPositiveSlope.java
+++ src/advanced_class7_DP4/LargestSetOfPointsWithPositiveSlope.java
@@ -2,7 +2,21 @@ package advanced_class7_DP4;
 
 import java.util.Arrays;
 import java.util.Comparator;
+/**
+ * 
+ * @author @Yifeng
+ * Given an array of 2D coordinates of points (all the coordinates are integers), 
+ * find the largest number of points that can form a set such that any pair of points 
+ * in the set can form a line with positive slope. Return the size of such a maximal set.
 
+	Assumptions
+	
+	The given array is not null
+	Note: if there does not even exist 2 points can form a line with positive slope, should return 0.
+	Examples
+	
+	<0, 0>, <1, 1>, <2, 3>, <3, 3>, the maximum set of points are {<0, 0>, <1, 1>, <2, 3>}, the size is 3.
+ */
 public class LargestSetOfPointsWithPositiveSlope {
 	public int largest(Point[] points) {
 		if (points == null || points.length <= 1) {
