433ea491b4d5bcb3ba037cf1eaef6c2fb812ce56
YifengGuo <gyf940517@gmail.com>
2017-08-26 10:54:26 -0400

    review and correct test of copyOfRange() api, right limit is excluded

--- src/advanced_class1_TwoPointers_KthValue/TestCopyOfRange.java
+++ src/advanced_class1_TwoPointers_KthValue/TestCopyOfRange.java
@@ -7,7 +7,7 @@ public class TestCopyOfRange {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int[] a = {1,2,3,4,5};
-		int[] b = Arrays.copyOfRange(a, 0, 3);//the left limit is excluded!!!
+		int[] b = Arrays.copyOfRange(a, 0, 3);//the right limit is excluded!!!
 		for(int i : b) {
 			System.out.print(i + " ");
 		}
