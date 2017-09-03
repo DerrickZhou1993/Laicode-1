fa586db9cf418867d68e0f9bfe0c86ccc4917b09
Yifeng Guo <gyf940517@gmail.com>
2017-09-03 11:50:15 -0400

    binary tree inorder traversal

--- src/TAclass4_Tree/BinaryTreeInorderTraversal.java
+++ src/TAclass4_Tree/BinaryTreeInorderTraversal.java
@@ -1,5 +1,53 @@
 package TAclass4_Tree;
 
-public class BinaryTreeInorderTraversal {
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.LinkedList;
+/**
+ * 
+ * @author yifengguo
+ 	Given a binary tree, return the inorder traversal of its nodes' values.
 
+	For example:
+	Given binary tree [1,null,2,3],
+	   1
+	    \
+	     2
+	    /
+	   3
+	return [1,3,2].
+ */
+
+/*
+ * iteration:
+			step1: new a Stack, initialize cur pointing to root
+			step2: as long as cur.left exists, keep pushing left child
+			step3: if no left child exists , stack.pop() and let cur point to right child
+			step4: keep iterating until cur == null || stack is empty
+ */
+/*
+ * time = O(n)
+ * space = O(n)
+ */
+public class BinaryTreeInorderTraversal {
+	public List<Integer> inorderTraversal(TreeNode root) {
+		List<Integer> res = new ArrayList<>();
+		if (root == null) {
+			return res;
+		}
+		Deque<TreeNode> stack = new LinkedList<>();
+		TreeNode cur = root;
+		while (cur != null || !stack.isEmpty()) {
+			if (cur != null) {
+				stack.push(cur);
+				cur = cur.left;
+			} else {
+				cur = stack.pop();
+				res.add(cur.key);
+				cur = cur.right;
+			}
+		}
+		return res;
+	}
 }
