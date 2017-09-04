a162db97692d2542c1abd96a63bddadbf9f6b235
Yifeng Guo <gyf940517@gmail.com>
2017-09-04 15:17:10 -0400

    more elegant way to determine balanced bianry tree with O(n) time

--- src/TAclass4_Tree/BalancedBinaryTree.java
+++ src/TAclass4_Tree/BalancedBinaryTree.java
@@ -8,20 +8,17 @@ package TAclass4_Tree;
 	in which the depth of the two subtrees of every node never differ by more than 1.
  */
 /*
- * time = O(nlogn)
- * space = O(height ^ 2)
+ * more elegant code for determining if a binary tree is balanced
+ * only need O(n) time and O(height) space
+ * recommended
  */
 public class BalancedBinaryTree {
 	public boolean isBalanced(TreeNode root) {
 		if (root == null) {
 			return true;
 		}
-		int left = getHeight(root.left);
-		int right = getHeight(root.right);
-		if (Math.abs(left - right) > 1) {
-			return false;
-		}
-		return isBalanced(root.left) && isBalanced(root.right);
+		int res = getHeight(root);
+		return res != -1;
 	}
 	
 	private int getHeight(TreeNode root) {
@@ -30,6 +27,39 @@ public class BalancedBinaryTree {
 		}
 		int left = getHeight(root.left);
 		int right = getHeight(root.right);
+		if (left == -1 || right == -1) {
+			return -1;
+		}
+		if (Math.abs(left - right) > 1) {
+			return -1;
+		}
 		return Math.max(left, right) + 1;
 	}
+	
+	
+	/*
+	 * time = O(nlogn)
+	 * space = O(height ^ 2)
+	 * nested recursion costs much time and space, not recommended
+	 */
+//	public boolean isBalanced(TreeNode root) {
+//		if (root == null) {
+//			return true;
+//		}
+//		int left = getHeight(root.left);
+//		int right = getHeight(root.right);
+//		if (Math.abs(left - right) > 1) {
+//			return false;
+//		}
+//		return isBalanced(root.left) && isBalanced(root.right);
+//	}
+//	
+//	private int getHeight(TreeNode root) {
+//		if (root == null) {
+//			return 0;
+//		}
+//		int left = getHeight(root.left);
+//		int right = getHeight(root.right);
+//		return Math.max(left, right) + 1;
+//	}
 }
