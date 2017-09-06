5cc2c924da326689e5f11783a7f8424b1d5c07e2
Yifeng Guo <gyf940517@gmail.com>
2017-09-05 22:58:41 -0400

    flatten a binary tree into linked list using recursion

---
+++ src/TAclass4_Tree/FlattenBinaryTreeToLinkedList.java
@@ -0,0 +1,78 @@
+package TAclass4_Tree;
+
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * 
+ * @author yifengguo
+ 	
+	Given a binary tree, flatten it to a linked list in-place.
+	
+	For example,
+	Given
+	
+	         1
+	        / \
+	       2   5
+	      / \   \
+	     3   4   6
+	The flattened tree should look like:
+	   1
+	    \
+	     2
+	      \
+	       3
+	        \
+	         4
+	          \
+	           5
+	            \
+	             6
+ */
+class Solution {
+	/*
+	 * elegant recursion
+	 */
+	public void flatten(TreeNode root) {
+		
+	}
+	
+	
+	
+	
+	
+	
+	/*
+	 * intuitive thinking use pre-order traversal of bianry tree
+	 * which is not concise and elegant enough
+	 * time = O(n)
+	 * space = O(n)
+	 */
+    public void flatten2(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+        Deque<TreeNode> stack = new LinkedList<>();
+        List<TreeNode> list = new ArrayList<>();
+        stack.push(root);
+        while (!stack.isEmpty()) {
+            TreeNode cur = stack.pop();
+            if (cur.right != null) {
+                stack.push(cur.right);
+            }
+            if (cur.left != null) {
+                stack.push(cur.left);
+            }
+            list.add(cur);
+        }
+        for (int i = 1; i < list.size(); i++) {
+            root.left = null;
+            root.right = list.get(i);
+            root = root.right;
+        }
+        return;
+    }
+}
