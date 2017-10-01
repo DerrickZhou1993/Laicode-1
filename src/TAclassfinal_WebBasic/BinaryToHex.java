f0bef75c8cd78018dcdd299630cbb751250f71f5
YifengGuo <gyf940517@gmail.com>
2017-10-01 10:35:32 -0400

    final TA class, computer and web basic

---
+++ src/TAclassfinal_WebBasic/BinaryToHex.java
@@ -0,0 +1,11 @@
+package TAclassfinal_WebBasic;
+/**
+ * 
+ * @author guoyifeng
+ 	transform binary number to hexadecimal number
+ */
+public class BinaryToHex {
+	public String binaryToHex(byte b) {
+		return null; 
+	}
+}
---
+++ src/TAclassfinal_WebBasic/DecodeByteArray.java
@@ -0,0 +1,5 @@
+package TAclassfinal_WebBasic;
+
+public class DecodeByteArray {
+
+}
