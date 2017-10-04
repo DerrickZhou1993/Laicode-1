ddbd7438c9cbc5bcc934b9ba2df6ff1cbe99cf9c
YifengGuo <gyf940517@gmail.com>
2017-10-04 14:21:28 -0400

    iterator

--- src/TAclass7_Iterator/TwoStepIterator.java
+++ src/TAclass7_Iterator/TwoStepIterator.java
@@ -1,4 +1,7 @@
 package TAclass7_Iterator;
+
+import java.util.NoSuchElementException;
+
 /**
  * 
  * @author @Yifeng
@@ -13,21 +16,31 @@ public class TwoStepIterator {
 	private int[] array;
 	private int index;
 	
-	// constrcutor
+	// constructor
 	public TwoStepIterator(int[] array) {
 		this.array = array;
-		index = 0;
+		index = -1;
 	}
 	
 	public boolean hasNext() {
-		
+		return index + 2 < array.length;
 	}
 	
 	public int next() {
+		if (!hasNext()) {
+			throw new NoSuchElementException("The array is empty or the index is out of bound");
+		}
+		int res =  array[index + 2];
+		index += 2;
+		return res;
 		
 	}
 	public static void main(String[] args) {
-
+		int[] array = new int[] {1, 2, 3, 4, 5};
+		TwoStepIterator it = new TwoStepIterator(array);
+		while (it.hasNext()) {
+			System.out.print(it.next() + " ");
+		}
 	}
 
 }
